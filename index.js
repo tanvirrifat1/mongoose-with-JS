e6bdf4e0b327102162813d94f1cd4102cebe7e3b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-24 10:31:19 +0600

    added single product

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000
 // middleWare
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 // create schema
 const productSchema = new mongoose.Schema({
     title: {
@@ -50,24 +51,54 @@ app.get('/', (req, res) => {
 
 app.post('/products', async (req, res) => {
     try {
-        // const newProduct = new Product({
-        //     title: req.body.title,
-        //     price: req.body.price,
-        //     description: req.body.description,
-        // })
-        const productData = await Product.insertMany([
-            {
-                title: "Redmi note8 pro",
-                price: 14656,
-                description: 'very smart and good looking phone'
-            },
-            {
-                title: "Google pixel phone ",
-                price: 784541,
-                description: 'very useful phone'
-            }
-        ])
+        const newProduct = new Product({
+            title: req.body.title,
+            price: req.body.price,
+            description: req.body.description,
+        })
+        const productData = await newProduct.save()
         res.status(201).send(productData)
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find()
+        if (products) {
+            res.status(200).send({
+                success: true,
+                message: 'return all product',
+                data: products
+            })
+        } else {
+            res.status(404).send({
+                success: false,
+                message: 'products not found'
+            })
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const product = await Product.findOne({ _id: id })
+        if (product) {
+            res.status(200).send({
+                success: true,
+                message: 'return single product',
+                data: product
+            })
+        } else {
+            res.status(404).send({
+                success: false,
+                message: 'products not found'
+            })
+        }
     } catch (error) {
         res.status(500).send({ message: error.message })
     }
